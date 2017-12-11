66438fb75686c4cc6204ca83fe0e1017c974ef6d
Olexandr P <polischuk@cinegy.com>
2017-12-11 16:25:00 +0200

    feat(invoices): add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { InvoicesComponent } from './invoices/invoices.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InvoicesComponent
   ],
   imports: [
     BrowserModule,
